33dc8453588dc09637475980d5cee8fff39a5446
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-03 17:48:49 -0800

    added timer and score

--- script.js
+++ script.js
@@ -22,91 +22,108 @@ let ans1 = document.querySelector("#ans1");
 let ans2 = document.querySelector("#ans2");
 let ans3 = document.querySelector("#ans3");
 let ans4 = document.querySelector("#ans4");
-let timer = document.querySelector ("#timer");
+let timer = document.querySelector("#timer");
 let scoring = document.querySelector('#scoring');
-let index = 0;
+let index = -1;
 let ansCount = 1;
-let secondsLeft = 15;
-let timeLeft= document.querySelector("#timeleft");
+let secondsLeft = 60;
+let timeLeft = document.querySelector("#timeleft");
 
 
 //Question and answers array
 
 const questionArr = [
-    {
-        question: "Best player?",
-        answer1: "Tom Brady",
-        answer2: "Drew Brees",
-        answer3: "Payton Manning",
-        answer4: "Tyrod Taylor",
-        correctAnswer: "Tom Brady",
-      },
-    
-      {
-        question: "Best Team?",
-        answer1: "Pats",
-        answer2: "Colts",
-        answer3: "Jags",
-        answer4: "Eagles",
-        correctAnswer: "Pats",
-      },
-    
-      {
-        question: "Best Player?",
-        answer1: "Lebron",
-        answer2: "MJ",
-        answer3: "Kobe",
-        answer4: "Tatum",
-        correctAnswer: "Lebron",
-      },
-    
-      {
-        question: "Best Team?",
-        answer1: "Celts",
-        answer2: "Yanks",
-        answer3: "Titans",
-        answer4: "Archers",
-        correctAnswer: "Celts",
-      },
-    
-      {
-        question: "Last major Championship?",
-        answer1: "Hockey",
-        answer2: "Football",
-        answer3: "Baseball",
-        answer4: "Basketball",
-        correctAnswer: "Baseball",
-      },
-    ];
+  {
+    question: "Best player?",
+    answer1: "Tom Brady",
+    answer2: "Drew Brees",
+    answer3: "Payton Manning",
+    answer4: "Tyrod Taylor",
+    correctAnswer: "Tom Brady",
+  },
+
+  {
+    question: "Best Team?",
+    answer1: "Pats",
+    answer2: "Colts",
+    answer3: "Jags",
+    answer4: "Eagles",
+    correctAnswer: "Pats",
+  },
+
+  {
+    question: "Best Player?",
+    answer1: "Lebron",
+    answer2: "MJ",
+    answer3: "Kobe",
+    answer4: "Tatum",
+    correctAnswer: "Lebron",
+  },
+
+  {
+    question: "Best Team?",
+    answer1: "Celts",
+    answer2: "Yanks",
+    answer3: "Titans",
+    answer4: "Archers",
+    correctAnswer: "Celts",
+  },
+
+  {
+    question: "Last major Championship?",
+    answer1: "Hockey",
+    answer2: "Football",
+    answer3: "Baseball",
+    answer4: "Basketball",
+    correctAnswer: "Baseball",
+  },
+];
 //Starts the quiz
-startBtn.addEventListener ("click", startQuiz);
-startBtn.addEventListener ("click", startTimer);
+startBtn.addEventListener("click", startQuiz);
+startBtn.addEventListener("click", startTimer);
 
 
-function startTimer(){
-setInterval(function () {
-    secondsLeft--;
-    timer.textContent = secondsLeft;
+function startTimer() {
+  setInterval(function () {
     if (secondsLeft > 0) {
-      
-    }else{
+      secondsLeft--;
+      localStorage.setItem("yourScore", secondsLeft);
+
+      timer.textContent = secondsLeft;
+
+    } if (index == questionArr.length) {
+      clearInterval();
+
+
+    }
+    if (secondsLeft == 0) {
+      window.location = "index2.html";
+      clearInterval();
       console.log("fin")
       // document.location.reload();
-      window.alert("All done!");
+
     }
   }, 1000);
 }
 
-function startQuiz(){
-  
-  if (questionArr[index] > questionArr.length) {
-    alert("Game Over");
-  } else {
-  question.textContent = questionArr[index].question;
-  ans1.textContent = questionArr[index].answer1;
-  ans2.textContent = questionArr[index].answer2;
-  ans3.textContent = questionArr[index].answer3;
-  ans4.textContent = questionArr[index].answer4;
+function startQuiz() {
+
+
+  index++
+
+if(index == questionArr.length){
+  window.location = "index2.html"
+  if(localStorage.getItem("yourScore") > localStorage.getItem("highScore") ){
+    localStorage.setItem("highScore", localStorage.getItem("yourScore"))
+  }
+}
+  if (index < questionArr.length) {
+    question.textContent = questionArr[index].question;
+    ans1.textContent = questionArr[index].answer1;
+    ans2.textContent = questionArr[index].answer2;
+    ans3.textContent = questionArr[index].answer3;
+    ans4.textContent = questionArr[index].answer4;
+
   }
 }
 //event listeners to track answer
@@ -116,38 +133,46 @@ ans3.addEventListener("click", checkAnswer);
 ans4.addEventListener("click", checkAnswer);
 
 //function called by button clicks above
-function checkAnswer(event){
+function checkAnswer(event) {
+
+
+
+
+  if (questionArr[index].correctAnswer == event.target.textContent) {
+    console.log("correct")
+
+
+  } else if (questionArr[index].correctAnswer != event.target.textContent) {
+    console.log("wrong");
+    secondsLeft = secondsLeft - 10
+  }
+  if (index == questionArr.length) {
+    window.location = "index2.html";
+    document.getElementById("questionBox").style.visibility = "hidden";
+    console.log("hey")
+  }
+
+
+
+
   console.log(questionArr[index].correctAnswer == event.target.textContent);
   console.log(event.target.textContent);
   console.log(questionArr[index].correctAnswer);
-  if (questionArr[index].correctAnswer == event.target.textContent){
-    console.log("correct")
-    console.log(ansCount ++);
-    ansCount ++
-
-  
-  }else{
-    console.log("wrong");
-    secondsLeft --
-  }
-  index++
   startQuiz();
 }
 
 
 
-function postScore (){
+function postScore() {
   if (secondsLeft == 0) {
     console.log("Finished!");
     scoring.append(ansCount)
+    console.log(ansCount);
 
-  } else {
-    console.log("close");
   }
-
 }
 
 postScore();
 
 
-  
+
---
+++ script2.js
@@ -0,0 +1,2 @@
+document.getElementById("highScore").append(localStorage.getItem("highScore"))
+document.getElementById("yourScore").append(localStorage.getItem("yourScore"))
